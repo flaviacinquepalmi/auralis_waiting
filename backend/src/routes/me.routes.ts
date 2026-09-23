a2f34a66bd622196ce21da0ebfae8ed60380e4e8
flaviacinquepalmi <148374704+flaviacinquepalmi@users.noreply.github.com>
2026-09-23 03:11:54 +0200

    Delete backend directory

--- backend/src/routes/me.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { requireAuth } from "../middleware/auth.middleware";
-import { syncAuth0User } from "../middleware/syncUser.middleware";
-
-export const meRouter = Router();
-
-meRouter.get("/", requireAuth, syncAuth0User, (req, res) => {
-  res.json({ user: req.dbUser });
-});
--- backend/src/server.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "./app";
-import { env } from "./config/env";
-import { logger } from "./utils/logger";
-
-app.listen(env.port, () => {
-  logger.info(`Server avviato su http://localhost:${env.port}`);
-});
